09bc9d49a97cd7a427a8bd01fae45ce8a5e63366
DariusLukasukas <64962012+DariusLukasukas@users.noreply.github.com>
2023-05-31 12:12:20 +0300

    update

--- components/sidebar/MediaItem.tsx
+++ components/sidebar/MediaItem.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { useLoadImage } from "@/hooks/useLoadImage";
 import { Song } from "@/types/types";
+import Image from "next/image";
 import React from "react";
 
 interface MediaItemProps {
@@ -15,5 +17,23 @@ export default function MediaItem({ data, onClick }: MediaItemProps) {
     }
     // TODO: Turn on player
   };
-  return <div onClick={handleClick}></div>;
+  return (
+    <div
+      onClick={handleClick}
+      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+    >
+      <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+        <Image
+          fill
+          src={imageUrl || "/images/liked.png"}
+          alt="Media Item"
+          className="object-cover"
+        />
+      </div>
+      <div className="flex flex-col gap-y-1 overflow-hidden">
+        <p className="text-white truncate">{data.title}</p>
+        <p className="text-neutral-400 text-sm truncate">{data.author}</p>
+      </div>
+    </div>
+  );
 }
